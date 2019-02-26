6e9e6cc1a17fd8082df3b5e7d51ff151966b079a
santoshkumarhunakunti <hunakunti1989@gmail.com>
2019-02-26 16:29:45 +0530

    grid example with better code plus

--- paint.js
+++ paint.js
@@ -1,36 +1,39 @@
-
+let selected=""
 let container = document.createElement('div')
 document.body.appendChild(container)
 container.className = 'col'
-for(let i=0;i<5;i++){
+for(let i=0;i<3;i++){
   let colGrid = document.createElement('div')
 
   colGrid.className = 'row'
-  for(let j=0;j<5;j++){
+  for(let j=0;j<3;j++){
     let grid = document.createElement('div')
     // grid.className = 'grid'
     grid.style.border = '1px solid'
     grid.style.height='100px'
     grid.style.width='100px'
     grid.onclick=()=>{
-      grid.style.backgroundColor= 'red'
+      grid.style.backgroundColor= selected
     }
     colGrid.appendChild(grid)
   }
     container.appendChild(colGrid)
 }
 
+let cls = ['green','red','blue','black','yellow']
 let colPaletteContainer = document.createElement('div')
 document.body.appendChild(colPaletteContainer)
 colPaletteContainer.className = 'Palette'
-for(let i=0;i<10;i++){
+for(let i=0;i<5;i++){
   let colour = document.createElement('div')
   colour.style.border = '1px solid'
   colour.style.height='100px'
   colour.style.width='100px'
+  colour.style.backgroundColor=cls[i]
   colPaletteContainer.appendChild(colour)
   colour.onclick=()=>{
   //colour.style.backgroundColor= 'red'
+  selected = cls[i]
   }
 }
 
