bdef7989313a63ed68b8b93f41ed66987e8d850d
santoshkumarhunakunti <hunakunti1989@gmail.com>
2019-02-26 16:03:15 +0530

    grid example with better code

--- paint.js
+++ paint.js
@@ -1,22 +1,23 @@
 
 let container = document.createElement('div')
 document.body.appendChild(container)
-container.style.height='100%'
-for(let i=0;i<4;i++){
+container.className = 'col'
+for(let i=0;i<10;i++){
   let colGrid = document.createElement('div')
-  container.appendChild(colGrid)
+
   colGrid.className = 'row'
-  for(let j=0;j<4;j++){
+  for(let j=0;j<10;j++){
     let grid = document.createElement('div')
+    // grid.className = 'grid'
+    grid.style.border = '1px solid'
     grid.style.height='100px'
     grid.style.width='100px'
-    grid.style.border.style='solid'
-    grid.style.border.color='green'
     grid.onclick=()=>{
       grid.style.backgroundColor= 'red'
     }
     colGrid.appendChild(grid)
   }
+    container.appendChild(colGrid)
 }
 // grid00.onclick=()=>{
 //   grid00.style.backgroundColor= 'red'
